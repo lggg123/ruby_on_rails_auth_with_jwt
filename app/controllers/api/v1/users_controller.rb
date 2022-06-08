77b8c53b208ba8156255cdb26dfd9fab33d4a3a8
lggg123 <machinelearner2334@gmail.com>
2022-06-08 16:05:58 -0700

    has_secure_password validation needed to be taken out in order for the user to save. Check and fix that error.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,20 +1,32 @@
 class Api::V1::UsersController < ApplicationController
-    def create
-        user = User.create(user_params)
-        if user.save
-            # UserRepresenter is a representer for cleaner code seen in representers
-            # check as_json representer as well on how to format the user when it is saved to the database with a token and this is the json
-            # representation we will see on screen.
-            # check representers/user_respresenter.rb for more architecture details
-            render json: UserRepresenter.new(user).as_json, status: :created
+    def index
+        @users = User.all
+        if @users
+            render json: { users: @users }
         else
-            @user.save
-            render json: { error: @user.errors.full_messsages }, status: :unproccessable_entity
+            render json: { status: 500, errors: ['no users found']}
         end
     end
 
+    def show
+        @user = User.find(params[:id])
+        if @user
+            render json: { user: @user }
+        else
+            render json: { status: 500, errors: ['user not found'] }
+        end
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render json: { status: :created, user: @user }
+        else
+            render json: { status: 500, errors: @user.errors.full_messages }
+        end
+    end
     private
     def user_params
-        params.require(:user).permit(:username, :password, :password_confirmation)
+        params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end
 end
